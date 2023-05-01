e5ca1d926175bd50eb9c3e7fa37ef22fad0487ce
MORGANJASON <420morganjason@gmail.com>
2023-05-01 05:17:08 +0300

    completed lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,47 @@
+import React, { useState } from "react";
 import React from "react";
 
-function Item({ name, category }) {
+function Item({ item }) {
+  const [inCart, setInCart] = useState(false);
+
+  function handleClick() {
+    setInCart(prevState => !prevState);
+  }
+
+  const cartText = inCart ? "Remove From Cart" : "Add to Cart";
+  const cartClass = inCart ? "in-cart" : "";
+
   return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+    <li className={cartClass}>
+      <img src={item.image} alt={item.name} />
+      <h4>{item.name}</h4>
+      <p>{item.description}</p>
+      <button onClick={handleClick}>{cartText}</button>
     </li>
   );
 }
 
 export default Item;
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+
+// function Item({ name, category }) {
+//   return (
+//     <li className="">
+//       <span>{name}</span>
+//       <span className="category">{category}</span>
+//       <button className="add">Add to Cart</button>
+//     </li>
+//   );
+// }
+
+// export default Item;
